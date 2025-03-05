951ff18916d1113cd1e614bd7e1cdca2e820d2fa
Remmane <ilyass.remmane@edu.esiee.fr>
2025-03-05 08:56:19 +0100

    premier comit

---
+++ C1/src/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "fraction.hpp"
+
+int main()
+{
+    Fraction f1 { 3, 4 };
+    Fraction f2 { 1, 2 };
+
+    std::cout << "f1 = ";
+    f1.display();
+    std::cout << " f2 = ";
+    f2.display();
+    std::cout << std::endl;
+
+    //Fraction f3 { add(f1, f2) };
+    Fraction f3 {f1 + f2};
+
+    std::cout << "add(f1, f2) = ";
+    f3.display();
+
+    std::cout << std::endl << "sub(f1, f2) = ";
+    //(sub(f1, f2)).display();
+    Fraction {f1-f2}.display();
+
+    std::cout << std::endl << "mul(f1, f2) = ";
+    //(mul(f1, f2)).display();
+    Fraction {f1*f2}.display();
+
+    std::cout << std::endl << "div(f1, f2) = ";
+    //(div(f1, f2)).display();
+    //Fraction {f1/f2}.display();
+    std::cout << Fraction {f1/f2} ;
+
+    std::cout << std::endl << "add(1/6, 2/6) = ";
+    // //(add({1, 6}, {2, 6})).display();
+    Fraction {Fraction {1, 6} + Fraction {2, 6}}.display();
+    //std::cout << Fraction {Fraction {1, 6} + Fraction {2, 6}};
+
+
+    return 0;
+}
